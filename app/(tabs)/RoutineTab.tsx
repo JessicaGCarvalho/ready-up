eba2173ef10ab4cac9456d312f0295c77f4a2e83
Jess Carvalho <carvalhojessicag@gmail.com>
2024-02-03 20:18:16 -0800

    skeleton

---
+++ app/(tabs)/RoutineTab.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import { styles } from "./styles";
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Routines</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+    </View>
+  );
+}
